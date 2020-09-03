e09ce9c930608f803a42d82e0ff32a863187746b
therandomfool <mddenver@protonmail.com>
2020-09-03 13:10:21 -0700

    JS solution find longest word

---
+++ app.js
@@ -0,0 +1,17 @@
+function longestWord(str) {
+
+    let words = str.split(" ");
+    let longestWord = '';
+    
+    for(let word of words) {
+        if (word.length > longestWord.length){
+            longestWord = word;
+        }
+    }
+
+    return longestWord;
+}
+
+
+console.log(longestWord("I woke up early today"));
+console.log(longestWord("I went stright to the beach"));
